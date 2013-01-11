f8a83b992052d5e3d7bb734190d82a25fa659ad8
Tim Watson <tim@rabbitmq.com>
2013-01-11 12:37:18 +0000

    implement AsyncSTM

--- tests/TestAsyncSTM.hs
+++ tests/TestAsyncSTM.hs
@@ -12,6 +12,7 @@ import Control.Distributed.Process
 import Control.Distributed.Process.Node
 import Control.Distributed.Process.Serializable()
 import Control.Distributed.Platform
+import Control.Distributed.Platform.Async
 import Control.Distributed.Platform.Async.AsyncSTM
 import Data.Binary()
 import Data.Typeable()
@@ -22,27 +23,27 @@ import Test.Framework (Test, testGroup)
 import Test.Framework.Providers.HUnit (testCase)
 import TestUtils
 
---testAsyncPoll :: TestResult (AsyncResult ()) -> Process ()
---testAsyncPoll result = do
---    hAsync <- async $ do "go" <- expect; say "running" >> return ()
---    ar <- poll hAsync
---    case ar of
---      AsyncPending ->
---        send (worker hAsync) "go" >> wait hAsync >>= stash result
---      _ -> stash result ar >> return ()
---      
---tests :: LocalNode  -> [Test]
---tests localNode = [
---    testGroup "Handling async results" [
---          testCase "testAsyncPoll"
---            (delayedAssertion
---             "expected poll to return a valid AsyncResult"
---             localNode (AsyncDone ()) testAsyncPoll) 
---      ]
---  ]
+testAsyncPoll :: TestResult (AsyncResult ()) -> Process ()
+testAsyncPoll result = do
+    hAsync <- async $ do "go" <- expect; say "running" >> return ()
+    ar <- poll hAsync
+    case ar of
+      AsyncPending ->
+        send (_asyncWorker hAsync) "go" >> wait hAsync >>= stash result
+      _ -> stash result ar >> return ()
+      
+tests :: LocalNode  -> [Test]
+tests localNode = [
+    testGroup "Handling async results" [
+          testCase "testAsyncPoll"
+            (delayedAssertion
+             "expected poll to return a valid AsyncResult"
+             localNode (AsyncDone ()) testAsyncPoll) 
+      ]
+  ]
 
 asyncStmTests :: NT.Transport -> IO [Test]
 asyncStmTests transport = do
-  -- localNode <- newLocalNode transport initRemoteTable
-  -- let testData = tests localNode
-  return [] -- testData
+  localNode <- newLocalNode transport initRemoteTable
+  let testData = tests localNode
+  return testData
